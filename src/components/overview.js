dbc666c964eb4854fbe45c7df0f335b51f69fa90
Jens-Konrad <jenspreem@gmail.com>
2018-12-14 11:59:03 +0200

    benefits overview data from BE

--- src/components/overview.js
+++ src/components/overview.js
@@ -16,8 +16,8 @@ class Overview extends Component {
   }
 
   render() {
-    const {userData: father, child} = this.props;
-    console.log(child);
+    const {benefits, child} = this.props;
+
     return (
       <div>
         <div>
@@ -25,43 +25,29 @@ class Overview extends Component {
             <h5>{child.firstName + " " + child.lastName}</h5>
           </div>
           <hr className="featurette-divider" />
+
           <div className="form-row">
             <div className="form-group col-md-3 text-align-right">Ema</div>
             <div className="form-group col-md-3 text-align-left">{child.mother ? child.mother.firstName + " " + child.mother.lastName : ""}</div>
-            <div className="form-group col-md-3 text-align-left">Sünnitoetus(Riik)</div>
-            <div className="form-group col-md-1 text-align-left cst-brdr">320€</div>
-            <div className="form-group col-md-2 text-align-left">
-              <button className="btn btn-outline-success">Makstud&nbsp;<FontAwesomeIcon icon="check" /></button>
-            </div>
           </div>
+
           <div className="form-row">
             <div className="form-group col-md-3 text-align-right">Isa</div>
             <div className="form-group col-md-3 text-align-left">{child.father ? child.father.firstName + " " + child.father.lastName : ""}</div>
-            <div className="form-group col-md-3 text-align-left">Sünnitoetus(KOV)</div>
-            <div className="form-group col-md-1 text-align-left cst-brdr">1000€</div>
-            <div className="form-group col-md-2 text-align-left">
-              <button className="btn btn-outline-success">Makstud&nbsp;<FontAwesomeIcon icon="check" /></button>
-            </div>
           </div>
+
           <div className="form-row">
             <div className="form-group col-md-3 text-align-right">Sünd</div>
             <div className="form-group col-md-3 text-align-left">{child.dateOfBirth || ''}</div>
-            <div className="form-group col-md-3 text-align-left">Lapsetoetus</div>
-            <div className="form-group col-md-1 text-align-left cst-brdr">55€</div>
-            <div className="form-group col-md-2 text-align-left">
-              <button className="btn btn-outline-warning">Menetlemisel&nbsp;<FontAwesomeIcon icon="exclamation-circle" /></button>
-            </div>
           </div>
+
+
           <div className="form-row">
             <div className="form-group col-md-3 text-align-right">Isikukood</div>
             <div className="form-group col-md-3 text-align-left">
               {child.idCode || ''}
             </div>
-            <div className="form-group col-md-3 text-align-left">Vanemahüvitis</div>
-            <div className="form-group col-md-1 text-align-left cst-brdr">1500€</div>
-            <div className="form-group col-md-2 text-align-left">
-              <button className="btn btn-outline-warning">Menetlemisel&nbsp;<FontAwesomeIcon icon="exclamation-circle" /></button>
-            </div>
+
           </div>
           <div className="form-row">
             <div className="form-group col-md-3 text-align-right">Lasteaed</div>
@@ -78,7 +64,24 @@ class Overview extends Component {
             <div className="form-group col-md-2 text-align-left"></div>
           </div>
         </div>
+
+        <hr className="featurette-divider" />
+
+        {benefits.map(b=>
+          <div className="form-row">
+            <div className="form-group col-md-3 text-align-left">{b.type})</div>
+            <div className="form-group col-md-1 text-align-left cst-brdr">{b.amount}</div>
+            <div className="form-group col-md-2 text-align-left">
+              {(b.status==="Makstud")? <button className="btn btn-outline-success">Makstud&nbsp;<FontAwesomeIcon icon="check" /></button>:
+                  (b.status==="Menetluses")?  <button className="btn btn-outline-warning">Menetlemisel&nbsp;<FontAwesomeIcon icon="exclamation-circle" /></button>:
+                      <button className="btn btn-outline-warning">Võimalik taodelda&nbsp;<FontAwesomeIcon icon="exclamation-circle" /></button>
+              }
+            </div>
+          </div>
+        )}
+
       </div>
+
     );
   }
 }
@@ -87,6 +90,7 @@ export default connect(
   state => ({
     userData: state.appUser.userData,
     child: state.childData.childData,
+    benefits: state.benefits.benefits,
   }),
   {fetchChildData, setToastSuccess, setToastError, fetchStepsData}
 )(Overview);
