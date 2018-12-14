2af2b74a2a233c809914832557778eeeba7e8e4c
lauriall <lauri.all@reach-u.com>
2018-12-14 12:59:24 +0200

    few styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,22 +7,17 @@ class Footer extends Component {
         <div className="container bottom_border">
           <div className="row">
             <div className=" col-sm-4 col-md col-sm-4  col-12 col">
-              <h5 className="headin5_amrc col_white_amrc pt2">Kontakt</h5>
-              <p className="mb10">Lorem Ipsum is simply dummy text of</p>
+              <h6 className="headin5_amrc col_white_amrc pt2">Kontakt</h6>
               <p><i className="fa fa-location-arrow"></i> Riia 35, Tallinn</p>
-              <p><i className="fa fa-phone"></i> +91-9999878398 </p>
-              <p><i className="fa fa fa-envelope"></i> info@example.com </p>
-
-
+              <p><i className="fa fa-phone"></i> +732-67832141 </p>
+              <p><i className="fa fa fa-envelope"></i> info@minusündmus.ee </p>
             </div>
 
             <div className=" col-sm-4 col-md  col-6 col">
-              <h5 className="headin5_amrc col_white_amrc pt2">Abi ning viited</h5>
+              <h6 className="headin5_amrc col_white_amrc pt2">Abi ning viited</h6>
               <ul className="footer_ul_amrc">
                 <li><a href="#">Abimaterjalid</a></li>
-                <li><a href="#">Shadows & Mirror Reflection</a></li>
-                <li><a href="#">Logo Design</a></li>
-                <li><a href="#">Vectorization</a></li>
+                <li><a href="#">Viited</a></li>
               </ul>
             </div>
           </div>
