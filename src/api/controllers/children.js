c23f2e8907cb183ebea66d843018201e595da117
lauriall <lauri.all@reach-u.com>
2019-01-04 16:30:04 +0200

    api endpoint error

--- src/api/controllers/children.js
+++ src/api/controllers/children.js
@@ -1,8 +1,8 @@
 import api from '../api';
 
 const children = {
-  get: () => api.get('/child/find'),
-  confirm: id => api.get(`/child/confirm_father/${id}`),
+  get: () => api.get('child/find'),
+  confirm: id => api.get(`child/confirm_father/${id}`),
 };
 
 export default children;
